171c214e53cbbf09c177417f04b730fad8128b26
alvinau <Alvin_Au@smartone.com>
2020-12-08 18:05:44 +0800

    Update commit

--- db-connector.js
+++ db-connector.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
 
-const uri = 'mongodb+srv://admin:admin@cluster0.c8zl3.mongodb.net/simple_chat?retryWrites=true';
-mongoose.connect(uri, { useNewUrlParser: true });
+const uri = 'mongodb://admin:admin@cluster0.c8zl3.mongodb.net/simple_chat?retryWrites=true&ssl=true&replicaSet=cluster0-shard-0&readPreference=secondary';
 
-mongoose.connection.on("error", function (error) {
-    console.log(error);
-});
+mongoose.connect(
+    uri,
+    { useNewUrlParser : true, useUnifiedTopology: true },
+    function(error) { 
+        if (error) { 
+            console.log(error); 
+        }
+    }
+); 
+      
 
-mongoose.connection.on("open", function () {
-    console.log("Connected to MongoDB database.");
-});
-
-// module.exports = connection;
+// module.exports = mongoose.connection;
