837528cac3f547ed8b5a67da4fc721501c3510ff
alvinau0427 <alvinau0427@outlook.com>
2020-12-10 06:55:07 +0800

    Bug fix commit

--- db-connector.js
+++ db-connector.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 
 // const uri = 'mongodb://localhost:27017/simple_chat';
-const uri = 'mongodb+srv://user:user@cluster0.c8zl3.mongodb.net/simple_chat?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://alvinau0427:78v7QCMbsdlX0Fnr@cluster0.c8zl3.mongodb.net/simple_chat?retryWrites=true&w=majority';
 
 mongoose.connect(
     uri,
-    { useNewUrlParser : true, useUnifiedTopology: true },
-    function(error) { 
-        if (error) { 
-            console.log(error); 
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err) {
+        if (err) {
+            console.log("System message: mongoose connection failed");
+        } else {
+            console.log("System message: mongosse connection success");
         }
     }
-); 
-      
+);
+
+mongoose.connection.on('error', console.error);
+
 module.exports = mongoose.connection;
