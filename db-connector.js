57a51b315feb29d22fafed5f600b1faba6390bf5
alvinau <Alvin_Au@smartone.com>
2020-12-14 10:28:47 +0800

    -- update mongoDB config

--- db-connector.js
+++ db-connector.js
@@ -3,11 +3,8 @@ const mongoose = require('mongoose');
 // Localhost MongoDB connection
 // const uri = 'mongodb://localhost:27017/simple_chat';
 
-// MongoDB Atlas connection
-const uri = 'mongodb+srv://alvinau0427:78v7QCMbsdlX0Fnr@cluster0.c8zl3.mongodb.net/simple_chat?retryWrites=true&w=majority';
-
 // Heroku key preference
-// const uri = process.env.MONGODB_URI;
+const uri = process.env.MONGODB_URI;
 
 mongoose.connect(
     uri,
